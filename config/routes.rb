8e18e7f0737e6d5fd4be9bd4dbf47507a8eca0c4
gabihuizar <gabriellacastaneda@gmail.com>
2021-04-05 21:18:49 -0700

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get '/todos', to: 'todos#index'
+  # get '/todos/:id' to: 'todos#show'
+  # post '/todos' to: 'todos#create'
+  # put '/todos/:id' to: 'todos#update'
+  # delete 'todos/:id' to: 'todos#destroy'
+end
